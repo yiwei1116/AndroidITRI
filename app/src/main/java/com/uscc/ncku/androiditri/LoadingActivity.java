469f68bca1fa9da5a02ff047932f93fa4409433f
kaizsv <kai.zsv@gmail.com>
2016-09-01 21:10:58 +0800

    test drawable multi screen

--- app/src/main/java/com/uscc/ncku/androiditri/LoadingActivity.java
+++ app/src/main/java/com/uscc/ncku/androiditri/LoadingActivity.java
@@ -3,7 +3,6 @@ package com.uscc.ncku.androiditri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 /**
  * Created by Lin on 2016/9/1.
@@ -15,7 +14,8 @@ public class LoadingActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
-        LinearLayout llayout = (LinearLayout) findViewById(R.id.llayout_loading);
+//        LinearLayout llayout = (LinearLayout) findViewById(R.id.llayout_loading);
         ImageView imgLoading = (ImageView) findViewById(R.id.img_loading);
+        imgLoading.setImageResource(R.drawable.logoGrey);
     }
 }
