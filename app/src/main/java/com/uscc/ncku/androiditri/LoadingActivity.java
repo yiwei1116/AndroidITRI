52c2ac733ab6048089eadd114151b669329fd5dd
kaizsv <kai.zsv@gmail.com>
2016-09-03 02:00:34 +0800

    complete logo loading animation
    to set the animation parameter
    
    1) LoadingActivity
    
    2) res/anim logo_fade_in and logo_fade_out

--- app/src/main/java/com/uscc/ncku/androiditri/LoadingActivity.java
+++ app/src/main/java/com/uscc/ncku/androiditri/LoadingActivity.java
@@ -2,25 +2,18 @@ package com.uscc.ncku.androiditri;
 
 import android.graphics.drawable.ClipDrawable;
 import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Lin on 2016/9/1.
  */
 public class LoadingActivity extends AppCompatActivity {
     /* time period(ms) while loading logo */
-    public static final int LOADING_PERIOD = 30;
+    public static final int LOADING_PERIOD = 40;
     /* loading level every time period from 0 to 10000 */
     public static final int LOADING_LEVEL = 200;
 
@@ -36,7 +29,7 @@ public class LoadingActivity extends AppCompatActivity {
     }
 
     /**
-     * Logo load animation.
+     * Logo loading animation.
      */
     private void logoLoading() {
         ImageView imgLoading = (ImageView) findViewById(R.id.img_loading);
@@ -51,6 +44,11 @@ public class LoadingActivity extends AppCompatActivity {
                     drawable.setLevel(drawable.getLevel() + LOADING_LEVEL);
                     handler.postDelayed(this, LOADING_PERIOD);
                 } else {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     dataLoading();
                 }
             }
@@ -58,8 +56,28 @@ public class LoadingActivity extends AppCompatActivity {
     }
 
     private void dataLoading() {
-        ImageView imgLoading = (ImageView) findViewById(R.id.img_loading);
-        imgLoading.setImageResource(R.drawable.living_logo);
+        final ImageView imgLoading = (ImageView) findViewById(R.id.img_loading);
+
+        final Animation fadeIn = AnimationUtils.loadAnimation(LoadingActivity.this, R.anim.logo_fade_in);
+        final Animation fadeOut = AnimationUtils.loadAnimation(LoadingActivity.this, R.anim.logo_fade_out);
+
+        imgLoading.startAnimation(fadeOut);
+
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) { }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                imgLoading.setImageResource(R.drawable.living_logo);
+                imgLoading.startAnimation(fadeIn);
+            }
+        });
     }
 
 }
