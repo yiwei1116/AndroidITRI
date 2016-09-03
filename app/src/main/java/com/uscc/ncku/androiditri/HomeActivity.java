5c150194e2958d3d972f2931574fce62285ab13e
kaizsv <kai.zsv@gmail.com>
2016-09-03 10:29:10 +0800

    add about activity

--- app/src/main/java/com/uscc/ncku/androiditri/HomeActivity.java
+++ app/src/main/java/com/uscc/ncku/androiditri/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.uscc.ncku.androiditri;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btnCht = (Button) findViewById(R.id.btn_cht_home);
+        Button btnEng = (Button) findViewById(R.id.btn_eng_home);
+
+        btnCht.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
