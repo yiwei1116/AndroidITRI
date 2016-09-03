8af3a5e5947cd61d1fc6e9667d2b0efa426576d1
kaizsv <kai.zsv@gmail.com>
2016-09-04 01:26:16 +0800

    reset about activity

--- app/src/main/java/com/uscc/ncku/androiditri/AboutActivity.java
+++ app/src/main/java/com/uscc/ncku/androiditri/AboutActivity.java
@@ -1,7 +1,11 @@
 package com.uscc.ncku.androiditri;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
 }
