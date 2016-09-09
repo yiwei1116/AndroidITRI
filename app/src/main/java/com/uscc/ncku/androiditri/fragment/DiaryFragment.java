7ff0796c129e880ddfe69882e3a94750e549a2c8
kaizsv <kai.zsv@gmail.com>
2016-09-10 00:05:34 +0800

    eleminate dead code

--- app/src/main/java/com/uscc/ncku/androiditri/fragment/DiaryFragment.java
+++ app/src/main/java/com/uscc/ncku/androiditri/fragment/DiaryFragment.java
@@ -11,23 +11,17 @@ import com.uscc.ncku.androiditri.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DiaryFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link DiaryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class DiaryFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-//    private OnFragmentInteractionListener mListener;
 
     public DiaryFragment() {
         // Required empty public constructor
@@ -66,43 +60,4 @@ public class DiaryFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_diary, container, false);
     }
-
-//    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-//
-//    /**
-//     * This interface must be implemented by activities that contain this
-//     * fragment to allow an interaction in this fragment to be communicated
-//     * to the activity and potentially other fragments contained in that
-//     * activity.
-//     * <p/>
-//     * See the Android Training lesson <a href=
-//     * "http://developer.android.com/training/basics/fragments/communicating.html"
-//     * >Communicating with Other Fragments</a> for more information.
-//     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
 }
