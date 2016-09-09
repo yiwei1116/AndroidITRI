b35ae149fed7be432264d76950a6a66374de9ce8
kaizsv <kai.zsv@gmail.com>
2016-09-10 03:09:17 +0800

    english button at home activity

--- app/src/main/java/com/uscc/ncku/androiditri/HomeActivity.java
+++ app/src/main/java/com/uscc/ncku/androiditri/HomeActivity.java
@@ -1,10 +1,15 @@
 package com.uscc.ncku.androiditri;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -27,7 +32,15 @@ public class HomeActivity extends AppCompatActivity {
         btnEng.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Locale locale = new Locale("en");
+                Resources res = getResources();
+                DisplayMetrics dm = res.getDisplayMetrics();
+                Configuration conf = res.getConfiguration();
+                conf.locale = locale;
+                res.updateConfiguration(conf, dm);
 
+                Intent intent = new Intent(HomeActivity.this, AboutActivity.class);
+                startActivity(intent);
             }
         });
 
