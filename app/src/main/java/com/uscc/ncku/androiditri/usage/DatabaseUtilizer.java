7c7034c78d8e1758af8c9d43be9198ada20d3a1a
Boopslo <oslophantom@gmail.com>
2016-09-27 23:46:21 +0800

    add frontend/backend communication

---
+++ app/src/main/java/com/uscc/ncku/androiditri/usage/DatabaseUtilizer.java
@@ -0,0 +1,33 @@
+package com.uscc.ncku.androiditri.usage;
+
+/**
+ * Created by Oslo on 9/27/16.
+ */
+public class DatabaseUtilizer {
+
+    /*
+        declare useful strings for downloading & uploading data
+     */
+    public static String serverURL = "http://140.116.82.48/phpmyadmin/index.php";
+    public static String queryPhp = "http://140.116.82.48/interface/deviceadd.php";
+
+    public static String dbProject = "project";
+    public static String dbFieldMap = "field_map";
+    public static String dbZone = "zone";
+    public static String dbPath = "path";
+    public static String dbBeacon = "beacon";
+    public static String dbMode = "mode";
+    public static String dbDevice = "device";
+    public static String dbCompany = "company";
+    public static String dbHipsterContent = "hipster_content";
+    public static String dbHipsterText = "hipster_text";
+    public static String dbHipsterTemplate = "hipster_template";
+    public static String dbVipDevice = "vip_device";
+    public static String dbVipVoice = "vip_voice";
+    public static String dbLease = "lease";
+    public static String dbUsers = "users";
+    public static String dbServerResult = "survey_result";
+    public static String dbVipPi = "vip_pi";
+    public static String dbSurvey = "survey";
+
+}
