5485acc4305a4168dc18b2d2a8169d55ba3fb624
Danny_Huang1116 <sm880508@gmail.com>
2016-09-29 12:42:38 +0800

    bug

--- app/src/main/java/com/uscc/ncku/androiditri/fragment/MergeTemplatePic.java
+++ app/src/main/java/com/uscc/ncku/androiditri/fragment/MergeTemplatePic.java
@@ -35,7 +35,7 @@ public class MergeTemplatePic extends Fragment {
     private String writeContext;
     private ImageView mergeImage;
     private TextView textView;
-    private OnFragmentInteractionListener mListener;
+
     private static final int[] Template_Image = {
             R.drawable.template_1,
             R.drawable.template_2,
@@ -95,29 +95,7 @@ public class MergeTemplatePic extends Fragment {
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
     /**
      * This interface must be implemented by activities that contain this
@@ -129,8 +107,5 @@ public class MergeTemplatePic extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+
 }
