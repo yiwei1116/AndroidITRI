f0150091676d84643b741ec90b4174e296cda32d
Boopslo <oslophantom@gmail.com>
2016-10-03 20:33:59 +0800

    create counter activity

---
+++ app/src/main/java/com/uscc/ncku/androiditri/CounterActivity.java
@@ -0,0 +1,13 @@
+package com.uscc.ncku.androiditri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CounterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+    }
+}
