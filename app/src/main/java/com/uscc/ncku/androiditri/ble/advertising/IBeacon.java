6d59437ccef9a354bb4ab3a487477338c7032ccf
JiaYing <s97531246@gmail.com>
2016-10-13 11:38:37 +0800

    modified MapFragment, add ble+sth webview need

---
+++ app/src/main/java/com/uscc/ncku/androiditri/ble/advertising/IBeacon.java
@@ -0,0 +1,378 @@
+/*
+ * Copyright (C) 2015 Neo Visionaries Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.uscc.ncku.androiditri.ble.advertising;
+
+
+import com.uscc.ncku.androiditri.ble.util.Bytes;
+import com.uscc.ncku.androiditri.ble.util.UUIDCreator;
+
+import java.util.UUID;
+
+
+/**
+ * iBeacon.
+ */
+public class IBeacon extends ADManufacturerSpecific
+{
+    private static final long serialVersionUID = 2L;
+    private static final int UUID_INDEX  =  4;
+    private static final int MAJOR_INDEX = 20;
+    private static final int MINOR_INDEX = 22;
+    private static final int POWER_INDEX = 24;
+    private static final String STRING_FORMAT =
+        "iBeacon(UUID=%s,Major=%d,Minor=%d,Power=%d)";
+
+    private UUID mUUID;
+    private int mMajor;
+    private int mMinor;
+    private int mPower;
+
+
+    /**
+     * Constructor which creates an instance with UUID=all-zeros,
+     * major=0, minor=0, and power=0.
+     */
+    public IBeacon()
+    {
+        this(26, 0xFF, new byte[] {
+            (byte)0x4C, (byte)0x00,   // Apple, Inc.
+            (byte)0x02, (byte)0x15,   // iBeacon
+
+            // Proximity UUID
+            (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00,
+            (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00,
+            (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00,
+            (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00,
+
+            (byte)0x00, (byte)0x00,   // Major number
+            (byte)0x00, (byte)0x00,   // Minor number
+            (byte)0x00                // Power
+        }, 0x004C);
+    }
+
+
+    /**
+     * Constructor.
+     *
+     * @param length
+     *         The length of the AD structure.
+     *
+     * @param type
+     *         The AD type. The value should always be 0xFF
+     *         (Manufacturer Specific Data).
+     *
+     * @param data
+     *         The AD data whose format is <i>iBeacon</i>.
+     *
+     * @param companyId
+     *         The company ID. The value should always be 0x004C
+     *         (Apple, Inc.).
+     *
+     * @throws IllegalArgumentException
+     *         {@code data} is {@code null} or the length of
+     *         {@code data} is less than 25.
+     */
+    public IBeacon(int length, int type, byte[] data, int companyId)
+    {
+        super(length, type, data, companyId);
+
+        parse(data);
+    }
+
+
+    /**
+     * Get the proximity UUID.
+     *
+     * @return
+     *         The proximity UUID.
+     */
+    public UUID getUUID()
+    {
+        return mUUID;
+    }
+
+
+    /**
+     * Set the proximity UUID.
+     *
+     *
+     * @param uuid
+     *         The proximity UUID. The value must not be {@code null}.
+     *
+     * @throws IllegalArgumentException
+     *         The given value is {@code null}.
+     */
+    public void setUUID(UUID uuid)
+    {
+        if (uuid == null)
+        {
+            throw new IllegalArgumentException("'uuid' is null.");
+        }
+
+        mUUID = uuid;
+
+        long msbits = uuid.getMostSignificantBits();
+        long lsbits = uuid.getLeastSignificantBits();
+
+        byte[] data = getData();
+        data[UUID_INDEX +  0] = (byte)((msbits >> 56) & 0xFF);
+        data[UUID_INDEX +  1] = (byte)((msbits >> 48) & 0xFF);
+        data[UUID_INDEX +  2] = (byte)((msbits >> 40) & 0xFF);
+        data[UUID_INDEX +  3] = (byte)((msbits >> 32) & 0xFF);
+        data[UUID_INDEX +  4] = (byte)((msbits >> 24) & 0xFF);
+        data[UUID_INDEX +  5] = (byte)((msbits >> 16) & 0xFF);
+        data[UUID_INDEX +  6] = (byte)((msbits >>  8) & 0xFF);
+        data[UUID_INDEX +  7] = (byte)((msbits      ) & 0xFF);
+        data[UUID_INDEX +  8] = (byte)((lsbits >> 56) & 0xFF);
+        data[UUID_INDEX +  9] = (byte)((lsbits >> 48) & 0xFF);
+        data[UUID_INDEX + 10] = (byte)((lsbits >> 40) & 0xFF);
+        data[UUID_INDEX + 11] = (byte)((lsbits >> 32) & 0xFF);
+        data[UUID_INDEX + 12] = (byte)((lsbits >> 24) & 0xFF);
+        data[UUID_INDEX + 13] = (byte)((lsbits >> 16) & 0xFF);
+        data[UUID_INDEX + 14] = (byte)((lsbits >>  8) & 0xFF);
+        data[UUID_INDEX + 15] = (byte)((lsbits >>  0) & 0xFF);
+    }
+
+
+    /**
+     * Get the major number.
+     *
+     * @return
+     *         The major number.
+     */
+    public int getMajor()
+    {
+        return mMajor;
+    }
+
+
+    /**
+     * Set the major number.
+     *
+     * @param major
+     *         The major number. The value should be in the range from 0 to 65535.
+     *
+     * @throws IllegalArgumentException
+     *         The given value is out of the valid range.
+     */
+    public void setMajor(int major)
+    {
+        if (major < 0 || 0xFFFF < major)
+        {
+            throw new IllegalArgumentException("'major' is out of the valid range: " + major);
+        }
+
+        mMajor = major;
+
+        byte[] data = getData();
+        data[MAJOR_INDEX    ] = (byte)((major >> 8) & 0xFF);
+        data[MAJOR_INDEX + 1] = (byte)((major     ) & 0xFF);
+    }
+
+
+    /**
+     * Get the minor number.
+     *
+     * @return
+     *         The minor number.
+     */
+    public int getMinor()
+    {
+        return mMinor;
+    }
+
+
+    /**
+     * Set the minor number.
+     *
+     * @param minor
+     *         The minor number. The value should be in the range from 0 to 65535.
+     *
+     * @throws IllegalArgumentException
+     *         The given value is out of the valid range.
+     */
+    public void setMinor(int minor)
+    {
+        if (minor < 0 || 0xFFFF < minor)
+        {
+            throw new IllegalArgumentException("'minor' is out of the valid range: " + minor);
+        }
+
+        mMinor = minor;
+
+        byte[] data = getData();
+        data[MINOR_INDEX    ] = (byte)((minor >> 8) & 0xFF);
+        data[MINOR_INDEX + 1] = (byte)((minor     ) & 0xFF);
+    }
+
+
+    /**
+     * Get the power.
+     *
+     * @return
+     *         The power.
+     */
+    public int getPower()
+    {
+        return mPower;
+    }
+
+
+    /**
+     * Set the power.
+     *
+     * @param power
+     *         The power. The value should be in the range from -128 to 127.
+     *
+     * @throws IllegalArgumentException
+     *         The given value is out of the valid range.
+     */
+    public void setPower(int power)
+    {
+        if (power < -128 || 127 < power)
+        {
+            throw new IllegalArgumentException("'power' is out of the valid range: " + power);
+        }
+
+        mPower = power;
+
+        getData()[POWER_INDEX] = (byte)(power & 0xFF);
+    }
+
+
+    private void parse(byte[] data)
+    {
+        // 25 = 2 (company ID) + 2 (format ID) + 16 (UUID) + 2 (major) + 2 (minor) + 1 (power)
+        if (data == null || data.length < 25)
+        {
+            throw new IllegalArgumentException("The byte sequence cannot be parsed as an iBeacon.");
+        }
+
+        mUUID  = buildUUID(data);
+        mMajor = buildMajor(data);
+        mMinor = buildMinor(data);
+        mPower = buildPower(data);
+    }
+
+
+    private UUID buildUUID(byte[] data)
+    {
+        return UUIDCreator.from128(data, 4, false);
+    }
+
+
+    private int buildMajor(byte[] data)
+    {
+        return Bytes.parseBE2BytesAsInt(data, MAJOR_INDEX);
+    }
+
+
+    private int buildMinor(byte[] data)
+    {
+        return Bytes.parseBE2BytesAsInt(data, MINOR_INDEX);
+    }
+
+
+    private int buildPower(byte[] data)
+    {
+        return data[POWER_INDEX];
+    }
+
+
+    /**
+     * Create an {@link IBeacon} instance.
+     *
+     * <p>
+     * The format of {@code data} should be as described in the following table.
+     * </p>
+     *
+     * <table border="1" style="border-collapse: collapse;" cellpadding="5">
+     *   <thead>
+     *     <tr>
+     *       <th></th>
+     *       <th>Value</th>
+     *       <th>Description</th>
+     *     </tr>
+     *   </thead>
+     *   <tbody>
+     *     <tr>
+     *       <td>Company ID</td>
+     *       <td><code>0x4C 0x00</code></td>
+     *       <td>The company ID assigned to Apple, Inc. (Little Endian)</td>
+     *     </tr>
+     *     <tr>
+     *       <td>Format ID</td>
+     *       <td><code>0x02 0x15</code></td>
+     *       <td>The format ID which represents <i>iBeacon</i></td>
+     *     </tr>
+     *     <tr>
+     *       <td>Proximity UUID</td>
+     *       <td>16-byte data</td>
+     *       <td>Proximity UUID</td>
+     *     </tr>
+     *     <tr>
+     *       <td>Major number</td>
+     *       <td>2-byte data</td>
+     *       <td>Major number (Big Endian)</td>
+     *     </tr>
+     *     <tr>
+     *       <td>Minor number</td>
+     *       <td>2-byte data</td>
+     *       <td>Minor number (Big Endian)</td>
+     *     </tr>
+     *     <tr>
+     *       <td>Power</td>
+     *       <td>1-byte data</td>
+     *       <td>The 2's complement of the calibrated Tx Power</td>
+     *     </tr>
+     *   </tbody>
+     * </table>
+     *
+     * @param length
+     *         The length of the AD structure.
+     *
+     * @param type
+     *         The AD type. The value should always be 0xFF which represents
+     *         <i>Manufacturer Specific Data</i>.
+     *
+     * @param data
+     *         The AD type. The value of the first two bytes is the company ID.
+     *
+     * @param companyId
+     *         The company ID. The value should always be 0x004C which represents
+     *         <i>Apple, Inc.</i>
+     *
+     * @return
+     *         An {@link IBeacon} instance. {@code null} is returned if the
+     *         length of {@code data} is less than 25.
+     */
+    public static IBeacon create(int length, int type, byte[] data, int companyId)
+    {
+        if (data == null || data.length < 25)
+        {
+            return null;
+        }
+
+        return new IBeacon(length, type, data, companyId);
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return String.format(STRING_FORMAT, mUUID, mMajor, mMinor, mPower);
+    }
+}
--- app/src/main/java/com/uscc/ncku/androiditri/fragment/MapFragment.java
+++ app/src/main/java/com/uscc/ncku/androiditri/fragment/MapFragment.java
@@ -1,11 +1,19 @@
 package com.uscc.ncku.androiditri.fragment;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Dialog;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,11 +22,20 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.uscc.ncku.androiditri.JavaScriptInterface;
 import com.uscc.ncku.androiditri.MainActivity;
 import com.uscc.ncku.androiditri.R;
+import com.uscc.ncku.androiditri.ble.BLEModule;
+import com.uscc.ncku.androiditri.ble.BLEScannerWrapper;
+import com.uscc.ncku.androiditri.util.DownloadProject;
 
 
 /**
@@ -30,6 +47,24 @@ public class MapFragment extends Fragment {
     private static final String TOUR_INDEX = "TOUR_INDEX";
 
     private int tourIndex;
+
+    private static final String TAG = "MapFragment";
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+    JavaScriptInterface mJavaScriptInterface;
+    private WebView mWebViewMap;
+    private String mSvgFile = "";
+    private int mScanedField;
+    private BLEScannerWrapper mBLEScannerWrapper;
+    private DownloadProject.Beacon mLastSacnBeacon = null;
+
+    private DownloadProject mTourProject;
+
+    //test
+    private String lastbeacon_mac;
+    private boolean t;
+    TextView address0;
+    ///
 
     public MapFragment() {
     }
@@ -49,6 +84,7 @@ public class MapFragment extends Fragment {
         return fragment;
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,6 +98,26 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mWebViewMap = (WebView) v.findViewById(R.id.webview_map);
+        address0 = (TextView)v.findViewById(R.id.device_address0);
+
+        //test
+        lastbeacon_mac = "";
+        mSvgFile = "living_1f.svg";
+        ///
+        mTourProject = new DownloadProject();
+        mTourProject.mIsDownloadCompleted =true;
+
+        buildBLEScannerWrapper();
+        initWebView();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android M Permission check
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+            }
+        }
 
         Toolbar toolbar = MainActivity.getToolbar();
         setHasOptionsMenu(true);
@@ -123,4 +179,198 @@ public class MapFragment extends Fragment {
         Toolbar toolbar = MainActivity.getToolbar();
         toolbar.setNavigationIcon(R.drawable.btn_back);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mBLEScannerWrapper.startBLEScan();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mBLEScannerWrapper.stopBLEScan();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // TODO request success
+                }
+                break;
+        }
+    }
+
+    private void initWebView() {
+        mWebViewMap.setWebChromeClient(new WebChromeClient());
+        mWebViewMap.setWebViewClient(new WebViewClient());
+        mJavaScriptInterface = new JavaScriptInterface(this);
+        mWebViewMap.addJavascriptInterface(mJavaScriptInterface, "Android");
+        mWebViewMap.setHorizontalScrollBarEnabled(false);
+        mWebViewMap.setVerticalScrollBarEnabled(false);
+        mWebViewMap.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        mWebViewMap.setBackgroundColor(Color.TRANSPARENT);
+
+        WebSettings websettings = mWebViewMap.getSettings();
+        websettings.setJavaScriptEnabled(true);
+        websettings.setSupportZoom(false);  // do not remove this
+        websettings.setAllowFileAccessFromFileURLs(true); // do not remove this
+        websettings.setSupportMultipleWindows(false);
+        websettings.setJavaScriptCanOpenWindowsAutomatically(false);
+        websettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        websettings.setLoadWithOverviewMode(true);
+        websettings.setUseWideViewPort(true);
+
+        String aURL = "file:///android_asset/index.html";
+        String scriptHtml = "<script>document.location =\"" + aURL + "\";</script>";
+        mWebViewMap.loadDataWithBaseURL(aURL, scriptHtml, "text/html", "utf-8", null);
+    }
+
+    private void buildBLEScannerWrapper(){
+        try {
+            mBLEScannerWrapper = new BLEScannerWrapper(this.getActivity(), bleHandler);
+            mBLEScannerWrapper.setScanDeviceName("CC2541BLEBroadcaster");
+            // from 0 to 100
+            mBLEScannerWrapper.setPowerLevelWarningThreshold(10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private final Handler bleHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            try {
+                String mac = "";
+                switch (msg.what) {
+                    case BLEModule.BLE_SCAN_DONE:
+                        // get region number from mac address (device.getAddress())
+                        if (mBLEScannerWrapper.getTheHighestRssiDevice() != null) {
+                            mac = mBLEScannerWrapper.getTheHighestRssiDevice().getAddress();
+                            if(mac == null)
+                                return;
+                            Log.d("Highest RSSI Mac = ", mac);
+                        }
+                        if (mBLEScannerWrapper.getTheLowestRssiDevice() != null) {
+                            Log.d("Lowest RSSI Mac = ", mBLEScannerWrapper.getTheLowestRssiDevice().getAddress());
+                        }
+                        if(!mTourProject.mIsDownloadCompleted) {
+                            // Project not download yet.
+                            return;
+                        }
+                        if(mLastSacnBeacon != null){
+                            if(mac.equals(mLastSacnBeacon.mac_addr)){
+                                // 跟上次的Beacon 一樣
+                                return;
+                            }
+                        }
+                        ///////////////////////////////
+
+                        address0.setText("\n\n\n當前偵測到的address: "+(mac.equals("")?"無":mac)+"\n上一個偵測到的address: "+(lastbeacon_mac.equals("")?"無":lastbeacon_mac));
+                        if(!mac.equals("") && !mac.equals(lastbeacon_mac))
+                        {
+                            lastbeacon_mac = mac;
+                            if(mac.equals("7C:EC:79:E5:C1:30")) {
+                                mWebViewMap.loadUrl("javascript: " + mJavaScriptInterface.getOnRegionChanged() + "(" + "2" + ")");
+                            }
+                            else if(mac.equals("98:7B:F3:5B:27:64")) {
+                                mWebViewMap.loadUrl("javascript: " + mJavaScriptInterface.getOnRegionChanged() + "(" + "3" + ")");
+                            }
+                            else if(mac.equals("98:7B:F3:5B:28:8D")) {
+                                mWebViewMap.loadUrl("javascript: " + mJavaScriptInterface.getOnRegionChanged() + "(" + "4" + ")");
+                            }
+                            else if(mac.equals("7C:EC:79:E5:C3:77")) {
+                                mWebViewMap.loadUrl("javascript: " + mJavaScriptInterface.getOnRegionChanged() + "(" + "5" + ")");
+                            }
+                        }
+                        ////////////////////////////////
+                        // query region id from database
+                        DownloadProject.Beacon b = GetZoneNum(mac);
+                        if (b == null) {
+                            return;
+                        }
+                        Log.d("ZontActivity", "b.zone.num = " + b.zone_num);
+                        if (mLastSacnBeacon == null || mLastSacnBeacon.field != b.field) {
+                            // Change field
+                            for (DownloadProject.Field f : mTourProject.mFieldList){
+                                if(f.id == b.field){
+                                    mScanedField = f.id;
+                                    String loadfile =  "file:///" + mTourProject.getStoragePath() + f.map_name ;
+                                    Log.d("ZontActivity", "javascript: setSVGLoad('" + loadfile + "'," + b.zone_num + ")");
+                                    //mWebViewMap.loadUrl("javascript: setSVGLoad('" + loadfile + "'," + b.zone_num + ")");
+
+                                    break;
+                                }
+                            }
+                        } else {
+                            /*if (mJavaScriptInterface.getOnRegionChanged() != null && !mJavaScriptInterface.getOnRegionChanged().equals("")) {
+                                //mWebViewMap.loadUrl("javascript: " + mJavaScriptInterface.getOnRegionChanged() + "(" + b.zone_num + ")");
+                            }*/
+                        }
+
+                        // change map button status
+                        /*
+                        if (mCurrentViewMode != ViewMode.MAP) {
+                            if (mSelectZone.zone_num != b.zone_num) {
+                                ivMap.setBackgroundResource(R.mipmap.map_btn_flash);
+                                ivMap.startAnimation(mapAnimation);
+                            } else {
+                                ivMap.clearAnimation();
+                                ivMap.setBackgroundResource(R.mipmap.map_btn_inactive);
+                            }
+                        }
+                        */
+                        mLastSacnBeacon = b;
+                        break;
+                    case BLEModule.BLE_LOW_POWER_WARNING:
+
+                        break;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        private DownloadProject.Beacon GetZoneNum(String mac){
+            if(mac.length() == 0)
+                return null;
+            for(DownloadProject.Field f: mTourProject.mFieldList){
+                for(DownloadProject.Beacon b: f.mBeaconList){
+                    if(mac.equals(b.mac_addr)){
+                        return b;
+                    }
+                }
+            }
+            return null;
+        }
+    };
+    public Handler getJsHandler() {
+        return jsHandler;
+    }
+
+    private final Handler jsHandler =
+            new Handler() {
+                public void handleMessage(Message msg) {
+                    switch (msg.what) {
+                        case JavaScriptInterface.DOCUMENTLOAD:
+                            String js;
+
+                            if(mSvgFile.length() == 0){
+                                js = "javascript: loadSVGLater()";
+                            }else{
+                                //String loadfile =  "file:///" + mTourProject.getStoragePath() + mSvgFile ;
+                                String loadfile =  "file:///android_asset/" + mSvgFile ;
+                                js = "javascript: setSVGLoad('" + loadfile + "',-1)";
+                            }
+                            mWebViewMap.loadUrl(js);
+                            break;
+                        case JavaScriptInterface.REGION_CLICKED:
+                            //buildZoneView(msg.arg1);
+                            //switchViewMode(ViewMode.ZONE);
+
+                            //int reg =  msg.arg1+1;
+                            //mWebViewMap.loadUrl("javascript: " + mJavaScriptInterface.getOnRegionChanged() + "(" + reg + ")");
+                            break;
+                    }
+                }
+            };
 }
