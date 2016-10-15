74363e45661763adfe142f6dca082b30a8bba28d
yiwei1116 <sm880508@gmail.com>
2016-10-15 09:54:15 +0800

    update

--- app/src/main/java/com/uscc/ncku/androiditri/fragment/YoutubeFragment.java
+++ app/src/main/java/com/uscc/ncku/androiditri/fragment/YoutubeFragment.java
@@ -1,46 +1,52 @@
 package com.uscc.ncku.androiditri.fragment;
 
-import android.app.FragmentTransaction;
+
 import android.os.Bundle;
 import android.app.Fragment;
-
-
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
 import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerFragment;
 import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 import com.uscc.ncku.androiditri.R;
 
 
 public class YoutubeFragment extends Fragment {
+
     private static final String API_KEY = "AIzaSyDS_zahrRAmInZOVBT_gCRcR4oXa2kjzS0";
 
-    private static String VIDEO_ID = "EGy39OMyHzw";
+
+    private static String VIDEO_ID = "tYA6TSTBjQ0";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_you_tube_player_view, container, false);
+        View rootView = inflater.inflate(R.layout.you_tube_api, container, false);
 
 
-      YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
+        YouTubePlayerFragment youTubePlayerFragment = YouTubePlayerFragment.newInstance();
 
-       /* FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-        transaction.add(R.id.youtube_player, youTubePlayerFragment).commit();*/
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.add(R.id.youtube_layout, youTubePlayerFragment).commit();
+
 
         youTubePlayerFragment.initialize(API_KEY, new OnInitializedListener() {
+
 
             @Override
             public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
                 if (!wasRestored) {
-                    player.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT);
-                    player.loadVideo(VIDEO_ID);
-                    player.play();
+                   player.cueVideo(VIDEO_ID);
+                    Log.e("123","123");
                 }
             }
 
