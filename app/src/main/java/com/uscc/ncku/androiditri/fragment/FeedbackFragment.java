5f06dfd774e94d56eeb5d06b50d089429e74e7ae
kaizsv <kai.zsv@gmail.com>
2016-11-05 16:07:44 +0800

    modify survey and feedback. TODO: feedback

--- app/src/main/java/com/uscc/ncku/androiditri/fragment/FeedbackFragment.java
+++ app/src/main/java/com/uscc/ncku/androiditri/fragment/FeedbackFragment.java
@@ -25,8 +25,11 @@ import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.uscc.ncku.androiditri.CommunicationWithServer;
 import com.uscc.ncku.androiditri.MainActivity;
 import com.uscc.ncku.androiditri.R;
+
+import org.json.JSONException;
 
 /**
  * use this to call feedback popup dialog
@@ -57,6 +60,28 @@ public class FeedbackFragment extends Fragment {
     private String[] SPINNER_LIEST;
     private View view;
     private ScrollView scrollView;
+
+    private boolean isEnglish;
+
+    private CommunicationWithServer comm;
+    private int attitude = 0;
+    private int functionality = 0;
+    private int visual = 0;
+    private int operability = 0;
+    private int user_friendly = 0;
+    private int price = 0;
+    private int maintenance = 0;
+    private int safety = 0;
+    private int energy = 0;
+    private int first_choise = 0, second_choise = 0, third_choise = 0, fourth_choise = 0, fifth_choise = 0;
+    private int first_consider = 0, second_consider = 0, third_consider = 0, fourth_consider = 0, fifth_consider = 0;
+    private int subscription1 = 0;
+    private int subscription2 = 0;
+    private int subscription3 = 0;
+    private int install1 = 0, install2 = 0, install3 = 0, install4 = 0, install5 = 0;
+    private int impression1 = 0, impression2 = 0, impression3 = 0, impression4 = 0, impression5 = 0;
+    private int buy = 0;
+    private int reasonable_price = 0;
 
     public FeedbackFragment() {
     }
@@ -108,7 +133,10 @@ public class FeedbackFragment extends Fragment {
 
         scrollView = (ScrollView) view.findViewById(R.id.scrollview_feedback);
 
-        if (getResources().getString(R.string.feedback_title).equals("Feedback")) {
+        isEnglish = ((MainActivity) getActivity()).isEnglish();
+        comm = ((MainActivity) getActivity()).getCommunicationWithServer();
+
+        if (isEnglish) {
             SPINNER_LIEST = SPINNER_LIEST_2F_EN;
         } else {
             SPINNER_LIEST = SPINNER_LIEST_2F;
@@ -136,18 +164,25 @@ public class FeedbackFragment extends Fragment {
         // hide all view in feedback while destroy view
         RadioGroup rGroup = (RadioGroup) view.findViewById(R.id.radiogroup_feedback_1);
         rGroup.setVisibility(View.GONE);
+
         rGroup = (RadioGroup) view.findViewById(R.id.radiogroup_feedback_8);
         rGroup.setVisibility(View.GONE);
+
         RelativeLayout rLayout = (RelativeLayout) view.findViewById(R.id.flayout_feedback_2);
         rLayout.setVisibility(View.GONE);
+
         rLayout = (RelativeLayout) view.findViewById(R.id.flayout_feedback_3);
         rLayout.setVisibility(View.GONE);
+
         rLayout = (RelativeLayout) view.findViewById(R.id.flayout_feedback_4);
         rLayout.setVisibility(View.GONE);
+
         rLayout = (RelativeLayout) view.findViewById(R.id.flayout_feedback_5);
         rLayout.setVisibility(View.GONE);
+
         rLayout = (RelativeLayout) view.findViewById(R.id.flayout_feedback_6);
         rLayout.setVisibility(View.GONE);
+
         rLayout = (RelativeLayout) view.findViewById(R.id.flayout_feedback_7);
         rLayout.setVisibility(View.GONE);
     }
@@ -187,15 +222,15 @@ public class FeedbackFragment extends Fragment {
         question.setText(R.string.feedback_question_1);
 
         if (radioGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_1_very_support) {
-
+            attitude = 1;
         } else if (radioGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_1_support) {
-
+            attitude = 2;
         } else if (radioGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_1_soso) {
-
+            attitude = 3;
         } else if (radioGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_1_unsupport) {
-
+            attitude = 4;
         } else if (radioGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_1_very_unsupport) {
-
+            attitude = 5;
         }
 
         Button next = (Button) view.findViewById(R.id.btn_feedback_next);
@@ -236,106 +271,106 @@ public class FeedbackFragment extends Fragment {
 
         RadioGroup rGFunction = (RadioGroup) view.findViewById(R.id.radiogroup_feedback_function);
         if (rGFunction.getCheckedRadioButtonId() == R.id.rbtn_feedback_function_very_important) {
-
+            functionality = 1;
         } else if (rGFunction.getCheckedRadioButtonId() == R.id.rbtn_feedback_function_important) {
-
+            functionality = 2;
         } else if (rGFunction.getCheckedRadioButtonId() == R.id.rbtn_feedback_function_soso) {
-
+            functionality = 3;
         } else if (rGFunction.getCheckedRadioButtonId() == R.id.rbtn_feedback_function_unimportant) {
-
+            functionality = 4;
         } else if (rGFunction.getCheckedRadioButtonId() == R.id.rbtn_feedback_function_very_unimportant) {
-
+            functionality = 5;
         }
 
         RadioGroup rGBeauty = (RadioGroup) view.findViewById(R.id.radiogroup_feedback_beauty);
         if (rGBeauty.getCheckedRadioButtonId() == R.id.rbtn_feedback_beauty_very_important) {
-
+            visual = 1;
         } else if (rGBeauty.getCheckedRadioButtonId() == R.id.rbtn_feedback_beauty_important) {
-
+            visual = 2;
         } else if (rGBeauty.getCheckedRadioButtonId() == R.id.rbtn_feedback_beauty_soso) {
-
+            visual = 3;
         } else if (rGBeauty.getCheckedRadioButtonId() == R.id.rbtn_feedback_beauty_unimportant) {
-
+            visual = 4;
         } else if (rGBeauty.getCheckedRadioButtonId() == R.id.rbtn_feedback_beauty_very_unimportant) {
-
+            visual = 5;
         }
 
         RadioGroup rGOperation = (RadioGroup) view.findViewById(R.id.radiogroup_feedback_operability);
         if (rGOperation.getCheckedRadioButtonId() == R.id.rbtn_feedback_operability_very_important) {
-
+            operability = 1;
         } else if (rGOperation.getCheckedRadioButtonId() == R.id.rbtn_feedback_operability_important) {
-
+            operability = 2;
         } else if (rGOperation.getCheckedRadioButtonId() == R.id.rbtn_feedback_operability_soso) {
-
+            operability = 3;
         } else if (rGOperation.getCheckedRadioButtonId() == R.id.rbtn_feedback_operability_unimportant) {
-
+            operability = 4;
         } else if (rGOperation.getCheckedRadioButtonId() == R.id.rbtn_feedback_operability_very_unimportant) {
-
+            operability = 5;
         }
 
         RadioGroup rGHumility = (RadioGroup) view.findViewById(R.id.radiogroup_feedback_humility);
         if (rGHumility.getCheckedRadioButtonId() == R.id.rbtn_feedback_humility_very_important) {
-
+            user_friendly = 1;
         } else if (rGHumility.getCheckedRadioButtonId() == R.id.rbtn_feedback_humility_important) {
-
+            user_friendly = 2;
         } else if (rGHumility.getCheckedRadioButtonId() == R.id.rbtn_feedback_humility_soso) {
-
+            user_friendly = 3;
         } else if (rGHumility.getCheckedRadioButtonId() == R.id.rbtn_feedback_humility_unimportant) {
-
+            user_friendly = 4;
         } else if (rGHumility.getCheckedRadioButtonId() == R.id.rbtn_feedback_humility_very_unimportant) {
-
+            user_friendly = 5;
         }
 
         RadioGroup rGPrice = (RadioGroup) view.findViewById(R.id.radiogroup_feedback_price);
         if (rGPrice.getCheckedRadioButtonId() == R.id.rbtn_feedback_price_very_important) {
-
+            price = 1;
         } else if (rGPrice.getCheckedRadioButtonId() == R.id.rbtn_feedback_price_important) {
-
+            price = 2;
         } else if (rGPrice.getCheckedRadioButtonId() == R.id.rbtn_feedback_price_soso) {
-
+            price = 3;
         } else if (rGPrice.getCheckedRadioButtonId() == R.id.rbtn_feedback_price_unimportant) {
-
+            price = 4;
         } else if (rGPrice.getCheckedRadioButtonId() == R.id.rbtn_feedback_price_very_unimportant) {
-
+            price = 5;
         }
 
         RadioGroup rGMaintenance = (RadioGroup) view.findViewById(R.id.radiogroup_feedback_maintenance);
         if (rGMaintenance.getCheckedRadioButtonId() == R.id.rbtn_feedback_maintenance_very_important) {
-
+            maintenance = 1;
         } else if (rGMaintenance.getCheckedRadioButtonId() == R.id.rbtn_feedback_maintenance_important) {
-
+            maintenance = 2;
         } else if (rGMaintenance.getCheckedRadioButtonId() == R.id.rbtn_feedback_maintenance_soso) {
-
+            maintenance = 3;
         } else if (rGMaintenance.getCheckedRadioButtonId() == R.id.rbtn_feedback_maintenance_unimportant) {
-
+            maintenance = 4;
         } else if (rGMaintenance.getCheckedRadioButtonId() == R.id.rbtn_feedback_maintenance_very_unimportant) {
-
+            maintenance = 5;
         }
 
         RadioGroup rGSafe = (RadioGroup) view.findViewById(R.id.radiogroup_feedback_safetly);
         if (rGSafe.getCheckedRadioButtonId() == R.id.rbtn_feedback_safetly_very_important) {
-
+            safety = 1;
         } else if (rGSafe.getCheckedRadioButtonId() == R.id.rbtn_feedback_safetly_important) {
-
+            safety = 2;
         } else if (rGSafe.getCheckedRadioButtonId() == R.id.rbtn_feedback_safetly_soso) {
-
+            safety = 3;
         } else if (rGSafe.getCheckedRadioButtonId() == R.id.rbtn_feedback_safetly_unimportant) {
-
+            safety = 4;
         } else if (rGSafe.getCheckedRadioButtonId() == R.id.rbtn_feedback_safetly_very_unimportant) {
-
+            safety = 5;
         }
 
         RadioGroup rGEnergy = (RadioGroup) view.findViewById(R.id.radiogroup_feedback_energy);
         if (rGEnergy.getCheckedRadioButtonId() == R.id.rbtn_feedback_energy_very_important) {
-
+            energy = 1;
         } else if (rGEnergy.getCheckedRadioButtonId() == R.id.rbtn_feedback_energy_important) {
-
+            energy = 2;
         } else if (rGEnergy.getCheckedRadioButtonId() == R.id.rbtn_feedback_energy_soso) {
-
+            energy = 3;
         } else if (rGEnergy.getCheckedRadioButtonId() == R.id.rbtn_feedback_energy_unimportant) {
-
+            energy = 4;
         } else if (rGEnergy.getCheckedRadioButtonId() == R.id.rbtn_feedback_energy_very_unimportant) {
-
+            energy = 5;
         }
 
         Button next = (Button) view.findViewById(R.id.btn_feedback_next);
@@ -900,9 +935,9 @@ public class FeedbackFragment extends Fragment {
 
         RadioGroup rGroup = (RadioGroup) view.findViewById(R.id.radiogroup_feedback_7);
         if (rGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_7_no) {
-
+            buy = 1;
         } else if (rGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_7_yes) {
-
+            buy = 2;
         }
 
         Button next = (Button) view.findViewById(R.id.btn_feedback_next);
@@ -936,15 +971,15 @@ public class FeedbackFragment extends Fragment {
         });
 
         if (radioGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_price_10k) {
-
+            reasonable_price = 1;
         } else if (radioGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_price_50k) {
-
+            reasonable_price = 2;
         } else if (radioGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_price_100k) {
-
+            reasonable_price = 3;
         } else if (radioGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_price_200k) {
-
+            reasonable_price = 4;
         } else if (radioGroup.getCheckedRadioButtonId() == R.id.rbtn_feedback_price_over_200k) {
-
+            reasonable_price = 5;
         }
 
         TextView title = (TextView) view.findViewById(R.id.txt_feedback_title);
@@ -957,6 +992,20 @@ public class FeedbackFragment extends Fragment {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // update feedback to server
+                try {
+                    comm.uploadSecondSurveyData(attitude,
+                            functionality, visual, operability, user_friendly, price, maintenance, safety, energy,
+                            first_choise, second_choise, third_choise, fourth_choise, fifth_choise,
+                            first_consider, second_consider, third_consider, fourth_consider, fifth_consider,
+                            subscription1, subscription2, subscription3,
+                            install1, install2, install3, install4, install5,
+                            impression1, impression2, impression3, impression4, impression5,
+                            buy,
+                            reasonable_price);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 new FeedbackUpload().execute();
             }
         });
